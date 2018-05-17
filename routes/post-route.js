2eff17b16cd05757724171bfccff7b6290a93c69
bunsushi <tbunsu@gmail.com>
2018-05-16 20:06:20 -0400

    moved about and tour to partials and index page links = smooth scroll

--- routes/post-route.js
+++ routes/post-route.js
@@ -12,15 +12,6 @@ router.get("/", function(req, res) {
     res.render("index");
 });
 
-// About Page
-router.get("/about", function(req, res) {
-    res.render("about");
-});
-
-// How It Works Page
-router.get("/tour", function(req, res) {
-    res.render("tour");
-});
 // Dashboard
 router.get("/dashboard", function(req, res) {
     res.render("dashboard");
