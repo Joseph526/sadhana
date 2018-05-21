d010d7e411c8e39c210aa7e7f3b4906a285b7f60
bunsushi <tbunsu@gmail.com>
2018-05-21 16:28:02 -0400

    add task from DOM

--- routes/task-route.js
+++ routes/task-route.js
@@ -1,12 +1,16 @@
 module.exports = function (app, Task) {
 
-    // GET all tasks
+    // GET route for retrieving all tasks
     app.get("/api/tasks/todo", function (req, res) {
         Task.findAll({}).then(function(result) {
             res.json(result);
+        });
+    });
+
+    // POST route for creating a new task
+    app.post("/api/tasks/todo", function (req, res) {
+        Task.create(req.body).then(function(result) {
+            res.json(result);
         })
     })
-
-    // // POST a new task
-
 }
